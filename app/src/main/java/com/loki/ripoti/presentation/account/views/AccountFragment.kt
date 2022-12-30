4f8222be68b552fed1564801431f35ba8562549a
Sheldon Okware <sheldon.okware@lampnets.com>
2022-12-30 10:41:39 +0300

    add implicit intents to navigate

--- app/src/main/java/com/loki/ripoti/presentation/account/views/AccountFragment.kt
+++ app/src/main/java/com/loki/ripoti/presentation/account/views/AccountFragment.kt
@@ -1,5 +1,7 @@
 package com.loki.ripoti.presentation.account.views
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -37,6 +39,7 @@ class AccountFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setUpAccount()
+        implicitIntents()
     }
 
 
@@ -107,6 +110,32 @@ class AccountFragment : Fragment() {
             val email = jwt.getClaim("email").asString()!!
             val profile = Profile(email)
             userProfileViewModel.getUserProfile(profile)
+        }
+    }
+
+
+    private fun implicitIntents() {
+        val from = arrayOf("lsheldon645@gmail.com")
+
+        binding.makeRequestLayout.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, from)
+            }
+            if (intent.resolveActivity(requireContext().packageManager) != null) {
+                startActivity(intent)
+            }
+        }
+
+        val phoneNumber = "254725992494"
+
+        binding.callLayout.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL).apply {
+                data = Uri.parse("tel:$phoneNumber")
+            }
+            if (intent.resolveActivity(requireContext().packageManager) != null) {
+                startActivity(intent)
+            }
         }
     }
 }
